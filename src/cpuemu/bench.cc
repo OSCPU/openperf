32216faf137f22c0f40209bae1720679bc944e07
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-08-05 15:13:54 +0800

    Feat: integrated with build system.

---
+++ src/cpuemu/bench.cc
@@ -0,0 +1,24 @@
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+
+uint64_t uptime()
+{
+	return (io_read(AM_TIMER_UPTIME).us);
+}
+
+char *format_time(uint64_t us) {
+  static char buf[32];
+  uint64_t ms = us / 1000;
+  printf("us: %u, ms: %u\n", us, ms);
+  us -= ms * 1000;
+  printf("us: %u, ms: %u\n", us, ms);
+  assert(us < 1000);
+  int len = sprintf(buf, "%d.000", ms);
+  char *p = &buf[len - 1];
+  while (us > 0) {
+    *(p --) = '0' + us % 10;
+    us /= 10;
+  }
+  return buf;
+}
--- src/cpuemu/emu.cc
+++ src/cpuemu/emu.cc
@@ -1,31 +1,32 @@
 #define ETL_NO_SMALL_CHAR_SUPPORT 0
-#include <am.h>
-#include <klib.h>
 #include <SimTop.h>
+#include <am.h>
+#include <bench.h>
+#include <klib.h>
 
 extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 
 #define MAX_PROGRAM_SIZE ((&ramdisk_start) - (&ramdisk_end))
-void * program;
+void *program;
 int program_sz = 0;
 SimTop cpu;
 
-void load_program(){
+void load_program() {
 
   program = malloc(MAX_PROGRAM_SIZE);
 
   memset(&program, 0, sizeof(program));
-  //move the program to the memory
+  // move the program to the memory
   memcpy(&program, &ramdisk_start, &ramdisk_end - &ramdisk_start);
   program_sz = &ramdisk_end - &ramdisk_start;
-  
+
   return;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   load_program();
-  
+
   memcpy(cpu.mem.rdata_mem.mem, &program, program_sz);
   cpu.reset = UInt<1>(1);
   cpu.eval(false, false, false);
@@ -40,7 +41,7 @@ int main(int argc, char** argv) {
 
   bool dut_end = false;
   // uint64_t cycles = 0;
-  while(!dut_end) {
+  while (!dut_end) {
     cpu.eval(true, true, true);
 
     if (cpu.io_uart_out_valid) {
@@ -49,4 +50,9 @@ int main(int argc, char** argv) {
       // fflush(stdout);
     }
   }
+
+  uint64_t totaltime = uptime() - 2800000000;
+  printf("time: %s ms\n", format_time(totaltime));
+
+  return 0;
 }
